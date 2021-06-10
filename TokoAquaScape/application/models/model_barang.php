44894abb96c69861d5650a66b61273bc77121cbd
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-10 09:32:55 +0700

    Update model_barang.php

--- TokoAquaScape/application/models/model_barang.php
+++ TokoAquaScape/application/models/model_barang.php
@@ -4,5 +4,8 @@ class Model_barang extends CI_Model{
     public function tampil_data(){
         return $this->db->get('barang');
     }
+
+    public function tambah_barang($data, $table){
+        $this->db->insert($table,$data);
+    }
 }
-?>
