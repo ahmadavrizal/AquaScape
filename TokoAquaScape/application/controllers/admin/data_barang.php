90e3d0a910cdf2d87b2f729cb492352dd15f4357
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-10 09:30:43 +0700

    Update data_barang.php

--- TokoAquaScape/application/controllers/admin/data_barang.php
+++ TokoAquaScape/application/controllers/admin/data_barang.php
@@ -9,4 +9,36 @@ class Data_barang extends CI_Controller{
         $this->load->view('admin/data_barang', $data);
         $this->load->view('templates_admin/footer');
     }
+
+    public function tambah_aksi(){
+        $nama_barang = $this->input->post('nama_barang');
+        $keterangan = $this->input->post('keterangan');
+        $kategori = $this->input->post('kategori');
+        $harga = $this->input->post('harga');
+        $stok = $this->input->post('stok');
+        $gambar = $_FILES['gambar']['name'];
+        if ($gambar = ''){}else{
+            $config ['upload_path'] = './uploads';
+            $config ['allowed_types'] = 'jpg|jpeg|png|gif';
+
+            $this->load->library('upload', $config);
+            if(!$this->upload->do_upload('gambar')){
+                echo "Gambar error!";
+            }else{
+                $gambar = $this->upload->data('file_name');
+            }
+        }
+
+        $data = array(
+            'nama_barang' => $nama_barang,
+            'keterangan' => $keterangan,
+            'kategori' => $kategori,
+            'harga' => $harga,
+            'stok' => $stok,
+            'gambar' => $gambar
+        );
+
+        $this->model_barang->tambah_barang($data, 'barang');
+        redirect('admin/data_barang/index');
+    }
 }
