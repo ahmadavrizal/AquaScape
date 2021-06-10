07832850872f74e918d4e2f4a249dc454c3b9f88
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-10 09:06:50 +0700

    Update data_barang.php

--- TokoAquaScape/application/controllers/admin/data_barang.php
+++ TokoAquaScape/application/controllers/admin/data_barang.php
@@ -1,10 +1,12 @@
 <?php
 
 class Data_barang extends CI_Controller{
-    public function index(){
+    public function index()
+    {
+        $data['barang'] = $this->model_barang->tampil_data()->result();
         $this->load->view('templates_admin/header');
         $this->load->view('templates_admin/sidebar');
-        $this->load->view('admin/data_barang');
+        $this->load->view('admin/data_barang', $data);
         $this->load->view('templates_admin/footer');
     }
 }
