de71d39ec777728b067b842bbea1935e70ba4d3f
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-10 09:19:22 +0700

    Update data_barang.php

--- TokoAquaScape/application/views/admin/data_barang.php
+++ TokoAquaScape/application/views/admin/data_barang.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
-    <button class="btn btn-sm btn-primary mb-3"><i class="fa fa-plus fa-sm"></i> Tambah Barang</button>
-    
+    <button class="btn btn-sm btn-primary mb-3" data-toggle="modal" data-target="#tambah_barang"><i class="fa fa-plus fa-sm"></i> Tambah Barang</button>
+
     <table class="table table-bordered">
         <tr>
             <th>NO</th>
@@ -30,4 +30,52 @@
 
         <?php endforeach ?>
     </table>
+
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="tambah_barang" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Barang</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?php echo base_url(). 'admin/data_barang/tambah_aksi' ?>" method="post" endtype="multipart/form-data">
+            <div class="form-group">
+                <label>Nama Barang</label>
+                <input type="text" name="nama_barang" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Keterangan</label>
+                <input type="text" name="keterangan" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Kategori</label>
+                <input type="text" name="kategori" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Harga</label>
+                <input type="text" name="harga" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Stok</label>
+                <input type="text" name="stok" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Gambar</label><br>
+                <input type="file" name="gambar" class="form-control">
+            </div>
+
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+        <button type="button" class="btn btn-primary">Simpan</button>
+      </div>
+    </div>
+  </div>
 </div>
