a4f61483bf72e2d16d42cf30635c26d784d12529
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-10 11:41:36 +0700

    Update data_barang.php

--- TokoAquaScape/application/controllers/admin/data_barang.php
+++ TokoAquaScape/application/controllers/admin/data_barang.php
@@ -11,12 +11,12 @@ class Data_barang extends CI_Controller{
     }
 
     public function tambah_aksi(){
-        $nama_barang = $this->input->post('nama_barang');
-        $keterangan = $this->input->post('keterangan');
-        $kategori = $this->input->post('kategori');
-        $harga = $this->input->post('harga');
-        $stok = $this->input->post('stok');
-        $gambar = $_FILES['gambar']['name'];
+        $nama_barang    = $this->input->post('nama_barang');
+        $keterangan     = $this->input->post('keterangan');
+        $kategori       = $this->input->post('kategori');
+        $harga          = $this->input->post('harga');
+        $stok           = $this->input->post('stok');
+        $gambar         = $_FILES['gambar']['name'];
         if ($gambar = ''){}else{
             $config ['upload_path'] = './uploads';
             $config ['allowed_types'] = 'jpg|jpeg|png|gif';
@@ -30,15 +30,49 @@ class Data_barang extends CI_Controller{
         }
 
         $data = array(
-            'nama_barang' => $nama_barang,
-            'keterangan' => $keterangan,
-            'kategori' => $kategori,
-            'harga' => $harga,
-            'stok' => $stok,
-            'gambar' => $gambar
+            'nama_barang'   => $nama_barang,
+            'keterangan'    => $keterangan,
+            'kategori'      => $kategori,
+            'harga'         => $harga,
+            'stok'          => $stok,
+            'gambar'        => $gambar
         );
 
         $this->model_barang->tambah_barang($data, 'barang');
+        redirect('admin/data_barang/index');
+    }
+
+    public function edit($id)
+    {
+        $where = array('id_barang' =>$id);
+        $data['barang'] = $this->model_barang->edit_barang($where, 'barang')->result();
+        $this->load->view('templates_admin/header');
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/edit_barang', $data);
+        $this->load->view('templates_admin/footer');
+    }
+
+    public function update(){
+        $id                 = $this->input->post('id_barang');
+        $nama_barang        = $this->input->post('nama_barang');
+        $keterangan         = $this->input->post('keterangan');
+        $kategori           = $this->input->post('kategori');
+        $harga              = $this->input->post('harga');
+        $stok               = $this->input->post('stok');
+
+        $data = array(
+            'nama_barang'       => $nama_barang,
+            'keterangan'       => $keterangan,
+            'kategori'       => $kategori,
+            'harga'       => $harga,
+            'stok'       => $stok,
+        );
+
+        $where = array(
+            'id_barang' => $id
+        );
+
+        $this->model_barang->update_data($where,$data,'barang');
         redirect('admin/data_barang/index');
     }
 }
