37eba1bcae63c718f2b82e4ebcd126fc0b9550cc
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-12 08:34:46 +0700

    Update dashboard.php

--- TokoAquaScape/application/views/dashboard.php
+++ TokoAquaScape/application/views/dashboard.php
@@ -28,13 +28,13 @@
 
         <?php foreach($barang as $brg) : ?>
 
-            <div class="card ml-3" style="width: 16rem;">
+            <div class="card ml-3 mb-3" style="width: 16rem;">
                 <img src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title mb-1"><?php echo $brg->nama_barang ?></h5>
                     <small><?php echo $brg->keterangan ?></small><br>
                     <span class="badge rounded-pill bg-light text-dark mb-3">RP. <?php echo $brg->harga ?></span><br>
-                    <a href="#" class="btn btn-sm btn-primary">Tambah ke Keranjang</a>
+                    <?php echo anchor('dashboard/tambah_ke_keranjang/' .$brg->id_barang, '<div class="btn btn-sm btn-primary">Tambah Ke keranjang</div>') ?>
                     <a href="#" class="btn btn-sm btn-success">Detail</a>
                 </div>
             </div>
