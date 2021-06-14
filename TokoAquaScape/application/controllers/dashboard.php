ef40e6f65d98bb73d412962937da32e831c6635f
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-14 07:37:14 +0700

    Update dashboard.php

--- TokoAquaScape/application/controllers/dashboard.php
+++ TokoAquaScape/application/controllers/dashboard.php
@@ -25,4 +25,12 @@ class Dashboard extends CI_Controller{
     $this->cart->insert($data);
     redirect('dashboard');
     }
+
+    public function detail_keranjang()
+    {
+    $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('keranjang');
+        $this->load->view('templates/footer');
+    }
 }
