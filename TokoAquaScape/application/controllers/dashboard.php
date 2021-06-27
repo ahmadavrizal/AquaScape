228304b6ecb3ba3cc23b0670f62a07b4bfae9f18
Ahmad Avrizal Widiarso <80190058+ahmadavrizal@users.noreply.github.com>
2021-06-27 10:45:13 +0700

    Update dashboard.php

--- TokoAquaScape/application/controllers/dashboard.php
+++ TokoAquaScape/application/controllers/dashboard.php
@@ -47,4 +47,13 @@ class Dashboard extends CI_Controller{
         $this->load->view('pembayaran');
         $this->load->view('templates/footer');
     }
+    
+    public function proses_pesanan()
+    {
+        $this->cart->destroy();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('proses_pesanan');
+        $this->load->view('templates/footer');
+    }
 }
