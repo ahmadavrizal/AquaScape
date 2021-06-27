2af9f1a96ce5d8e89f5c1243d5535fd11f9614f5
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-27 20:56:11 +0700

    Update auth.php

--- TokoAquaScape/application/controllers/auth.php
+++ TokoAquaScape/application/controllers/auth.php
@@ -35,12 +35,18 @@ class Auth extends CI_Controller {
                     switch($auth->role_id){
                         case 1 : redirect('admin/Dashboard_admin');
                                  break;
-                        case 2 : redirect('dashboard');
+                        case 2 : redirect('welcome');
                             break;
                         default: break;
                 }
                 
             }
         }
+    }
+
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        redirect('auth/login');
     }
 }
