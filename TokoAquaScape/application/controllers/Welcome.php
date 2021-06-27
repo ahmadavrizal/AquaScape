e810aab0da7f22de094f6c163ffc40e219279ded
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-27 20:54:02 +0700

    Update Welcome.php

--- TokoAquaScape/application/controllers/Welcome.php
+++ TokoAquaScape/application/controllers/Welcome.php
@@ -19,7 +19,11 @@ class Welcome extends CI_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    {
+        $data['barang'] = $this->model_barang->tampil_data()->result();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('dashboard', $data);
+        $this->load->view('templates/footer');
+    }
 }
