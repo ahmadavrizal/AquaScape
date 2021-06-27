a0cab17b418dd4b8ef51c60803a0a9c9030082a5
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-27 20:56:44 +0700

    Update kategori.php

--- TokoAquaScape/application/controllers/kategori.php
+++ TokoAquaScape/application/controllers/kategori.php
@@ -1,6 +1,21 @@
 <?php
 
-class kategori extends CI_Controller{
+class Kategori extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+
+        if($this->session->userdata('role_id') != '2'){
+            $this->session-$this->session->set_flashdata('pesan','
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            Harap Login Terlebih dahulu!
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+            </div>');
+            redirect('auth/login');
+        }
+    }
     public function elektronik()
     {
         $data['elektronik'] = $this->model_kategori->data_elektronik()->result();
