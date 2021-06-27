eb1da93a8c754fb5107a01c5de4374b7d0e70f0e
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-27 16:51:11 +0700

    Update sidebar.php

--- TokoAquaScape/application/views/templates_admin/sidebar.php
+++ TokoAquaScape/application/views/templates_admin/sidebar.php
@@ -19,7 +19,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="<?php echo base_url('admin/dashboard_admin') ?>">
+                <a class="nav-link" href="<?php echo base_url('admin/Dashboard_admin') ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -103,37 +103,14 @@
                             </div>
                         </li>
 
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
-
+                        <ul class="nav navbar-nav navbar-right">
+                                    <?php if($this->session->userdata('username')) { ?>
+                        
+                                        <li class="ml-2"><?php echo anchor('auth/logout','Logout'); ?></li>
+                                    <?php } else { ?>
+                                        <li><?php echo anchor('auth/login', 'Login'); ?></li>
+                                    <?php } ?>
+                    </ul>
                     </ul>
 
                 </nav>
