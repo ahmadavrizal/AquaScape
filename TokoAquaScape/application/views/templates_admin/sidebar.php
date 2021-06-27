006d1ea33e5e4d93dc03bf3e8e601127751a2fd4
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-27 20:55:42 +0700

    Update sidebar.php

--- TokoAquaScape/application/views/templates_admin/sidebar.php
+++ TokoAquaScape/application/views/templates_admin/sidebar.php
@@ -4,7 +4,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
