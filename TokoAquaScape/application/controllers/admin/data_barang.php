266e85bc50949d24b0fc7e90971e7e6b7e5d1609
wildanaldip <83572867+wildanaldip@users.noreply.github.com>
2021-06-27 20:57:47 +0700

    Update data_barang.php

--- TokoAquaScape/application/controllers/admin/data_barang.php
+++ TokoAquaScape/application/controllers/admin/data_barang.php
@@ -1,6 +1,21 @@
 <?php
 
 class Data_barang extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+
+        if($this->session->userdata('role_id') != '1'){
+            $this->session-$this->session->set_flashdata('pesan','
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            Harap Login Terlebih dahulu!
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+            </div>');
+            redirect('auth/login');
+        }
+    }
     public function index()
     {
         $data['barang'] = $this->model_barang->tampil_data()->result();
